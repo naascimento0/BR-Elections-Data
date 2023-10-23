85aa26ed41b92c07715e64b5a90d6ec05e73f24a
lsalatiel <lucassalatiel1004@gmail.com>
2023-10-22 21:39:13 -0300

    add enums

---
+++ src/eleicao/Genero.java
@@ -0,0 +1,22 @@
+package eleicao;
+public enum Genero {
+    MASCULINO("2"),
+    FEMININO("4");
+
+    private String genero;
+
+    private Genero(String codigo) {
+        this.genero = codigo;
+    }
+
+    public String getCodigoGenero() {
+        return genero;
+    }
+
+    public static Genero getGenero(String codigoGenero) {
+        if(codigoGenero.equals(MASCULINO.genero))
+            return MASCULINO;
+        else
+            return FEMININO;
+    }
+}
